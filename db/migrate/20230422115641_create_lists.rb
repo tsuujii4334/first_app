71b6f96ab435b5b20614d91caed36e1764579eea
EC2 Default User <ec2-user@ip-172-31-35-45.ap-northeast-1.compute.internal>
2023-04-22 17:48:19 +0000

    3-5[finish]

--- db/migrate/20230422115641_create_lists.rb
+++ db/migrate/20230422115641_create_lists.rb
@@ -3,6 +3,7 @@ class CreateLists < ActiveRecord::Migration[6.1]
     create_table :lists do |t|
       t.string :title
       t.string :body
+      t.string :author
       t.timestamps
     end
   end
